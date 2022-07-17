473f215aef6a253cfc7b71b5b4763f1faa2cce24
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-17 23:00:54 +0530

    changes in server setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 
 //express app
 const app = express();
 
+app.get('/', (req, res) => {
+    res.json({message:'Welcome'})
+});
+
 //listen for request
-app.listen(4000, () => {
-    console.log('Listening on port 4000 !!');
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port :', process.env.PORT );
 })
