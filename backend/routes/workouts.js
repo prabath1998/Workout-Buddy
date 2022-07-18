c52f642930ad40e2a96ce2df5a3f95af8378ecba
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-18 09:11:15 +0530

    create workout api added

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Workout = require('../models/workoutModel')
 const router = express.Router();
 
 //Get all workouts
@@ -12,7 +13,16 @@ router.get('/:id',(req,res)=>{
 })
 
 //post new workout
-router.post('/',(req,res)=>{
+router.post('/',async(req,res)=>{
+    const { title, load, reps } = req.body;
+
+    try {
+        const workout =await Workout.create({title, load, reps});
+        res.status(200).json(workout);
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
     res.json({message:"post new workout"})
 });
 
