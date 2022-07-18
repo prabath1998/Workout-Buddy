56746966b42c5790e7e3c16951a69be1dad445ba
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-18 14:34:39 +0530

    minor changes

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import  WorkoutDetails from '../components/WorkoutDetails';
+import  WorkoutForm from '../components/WorkoutForm';
 
 const Home = () => {
 
@@ -16,7 +17,7 @@ const Home = () => {
         }
       } 
       fetchWorkouts()     
-    }, [])
+    }, [workouts])
     
 
     return ( 
@@ -26,6 +27,7 @@ const Home = () => {
                     <WorkoutDetails key={workout._id} workout={workout}/>
                 ))}
             </div>
+            <WorkoutForm />
         </div>
      );
 }
